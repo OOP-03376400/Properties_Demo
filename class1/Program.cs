55da9f538bbfb04e09045fb3c1d1196c013e00ea
Koson Trachu <koson.trachu@gmail.com>
2016-08-31 11:38:11 +0700

    properties demo

---
+++ class1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class1
+{
+    class money
+    {
+        public money(double bath) { this.amount = bath; } // constructor
+        public double amount; // field
+        public double bathToDollar() { return amount * 1d / 33d; }
+        public double dollar  // properties
+        {
+            get { return amount * 1d / 33d; }
+            set {; }
+        }
+    }
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            money mymoney = new money(50);
+            Console.WriteLine(mymoney.dollar);
+        }
+    }
+
+
+}
